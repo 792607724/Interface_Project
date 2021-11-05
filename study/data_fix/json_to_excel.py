7fe50bdee2b5380a83bff71244e605c8b9307974
Bruce <39899119+792607724@users.noreply.github.com>
2021-11-05 12:07:36 +0800

    Deal with the data and arrange it for good analysis

--- study/data_fix/json_to_excel.py
+++ study/data_fix/json_to_excel.py
@@ -11,17 +11,19 @@ cur_time = time.strftime("%Y%m%d")
 
 
 def json_to_excel(json_path, excel_path):
-    df = pd.DataFrame(columns=["number", "voltage"])
+    # df = pd.DataFrame(columns=["number", "voltage"])
+    df = pd.DataFrame(columns=["number", "rpm"])
     df.to_excel(excel_path, index=False)
     print("{} excel generate success!".format(excel_path))
 
     with open(json_path, "r") as json_file:
         json_data = json.load(json_file)
-    voltage_raw_data = json_data["vol"]
+    # voltage_raw_data = json_data["vol"]
+    rpm_raw_data = json_data["rpm"]
     df_2 = pd.read_excel(excel_path, header=None, engine="openpyxl")
     all_list = []
     i = 0
-    for voltage_item in voltage_raw_data:
+    for voltage_item in rpm_raw_data:
         i += 1
         temp_list = []
         temp_list.append(i)
@@ -33,6 +35,9 @@ def json_to_excel(json_path, excel_path):
 
 
 if __name__ == '__main__':
-    json_path = "./voltage_20211104152221.json"
-    excel_path = "./voltage_{}.xlsx".format(cur_time)
+    # json_path = "./voltage_20211105115538.json"
+    json_path = "./rpm_20211105115550.json"
+
+    # excel_path = "./voltage_{}.xlsx".format(cur_time)
+    excel_path = "./rpm_{}.xlsx".format(cur_time)
     json_to_excel(json_path, excel_path)
