4961502129e040b2202abfeda003684fec7c39da
Bruce <39899119+792607724@users.noreply.github.com>
2021-11-17 14:15:36 +0800

    Deal with the data and arrange it for good analysis

--- study/data_fix/json_to_excel.py
+++ study/data_fix/json_to_excel.py
@@ -1,7 +1,6 @@
 # coding = utf8
 
 import os
-import subprocess
 
 os.path.abspath(".")
 import time
@@ -57,4 +56,3 @@ if __name__ == '__main__':
     # excel_path = "./voltage_{}.xlsx".format(cur_time)
     excel_path = "./rpm_{}.xlsx".format(cur_time)
     json_to_excel(json_path, excel_path)
-
