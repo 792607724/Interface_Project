2996dc74fafa5614400fe05c41b7d43165751ba9
Bruce <39899119+792607724@users.noreply.github.com>
2021-11-20 16:47:25 +0800

    Deal with the data and arrange it for good analysis

---
+++ study/mysql_operate/opmysql.py
@@ -0,0 +1,23 @@
+# coding = utf8
+import os
+
+os.path.abspath(".")
+
+"""
+    定义对MySQL数据库基本操作的封装
+    1、包括基本的单条语句操作，如删除、修改、更新
+    2、独立地查询单条、多条语句
+    3、独立地添加多条语句
+"""
+import logging
+import pymysql
+from public import config
+
+print("MYSQL")
+
+
+
+
+
+
+
