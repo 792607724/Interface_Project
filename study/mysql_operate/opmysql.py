b670e13471e213d5014860c8c1bc631151661d08
Bruce <39899119+792607724@users.noreply.github.com>
2021-11-22 11:59:45 +0800

    Interface and MySQL learning codeing

--- study/mysql_operate/opmysql.py
+++ study/mysql_operate/opmysql.py
@@ -1,23 +1,75 @@
 # coding = utf8
 import os
 
+from study.mysql_operate import config
+
 os.path.abspath(".")
+"""
+    @Project:Interface_Project
+    @File:opmysql.py
+    @Author:十二点前要睡觉
+    @Date:2021/11/22 10:52
+"""
 
 """
-    定义对MySQL数据库基本操作的封装
-    1、包括基本的单条语句操作，如删除、修改、更新
-    2、独立地查询单条、多条语句
-    3、独立地添加多条语句
+    Define the package for operations of the MySQL database
+    1、Include the basic single operate statement,such like delete、modify、update
+    2、Independent query single、multiple statements
+    3、Independent add multiple statements
 """
 import logging
 import pymysql
-from public import config
 
-print("MYSQL")
-
-
-
-
-
+"""
+    the import is not FIND, after whole code input, 
+    see whether the config is used, 
+    now we can't confirm.
+"""
 
 
+# from public import config
+
+class OperationDbInterface(object):
+    # Define database init connection
+    def __init__(self, host_db="10.64.30.182", user_db="test",
+                 password_db="seevision", name_db="test_interface",
+                 port_db=3306, link_type=0):
+        """
+
+        @param host_db:database service host, if mysql in local, the host_db is the pc ip address
+        @param user_db:database user name
+        @param password_db:database password
+        @param name_db:database name
+        @param port_db:database port, integer type
+        @param link_type:connection type, to identify the output data is tuple or dictionary, default is dictionary, link_type=0
+        @param return:cursor
+        """
+        try:
+            if link_type == 0:
+                # Create database connection and return dictionary
+                self.conn = pymysql.connect(host=host_db,
+                                            user=user_db,
+                                            passwd=password_db,
+                                            db=name_db,
+                                            port=port_db,
+                                            charset="utf8",
+                                            cursorclass=pymysql.cursors.DictCursor)
+            else:
+                # Create database connection and return tuple
+                self.conn = pymysql.connect(host=host_db,
+                                            user=user_db,
+                                            passwd=password_db,
+                                            db=name_db,
+                                            port=port_db,
+                                            charset="utf8")
+            self.cur = self.conn.cursor()
+        except pymysql.Error as e:
+            print("Create database connection failed | Mysql Error %d: %s" % (e.args[0], e.args[1]))
+            logging.basicConfig(filename=config.src_path + "/log/system_error.log", level=logging.DEBUG,
+                                format="%(asctime)s_%(filename)s[line:%(lineno)d]_%(levelname)s %(message)s")
+            logger = logging.getLogger(__name__)
+            logger.exception(e)
+
+
+if __name__ == '__main__':
+    print(config.src_path)
