cb61fd8de1b8de1049239ac4eda6c9c9f70609f8
Bruce <39899119+792607724@users.noreply.github.com>
2021-11-22 16:27:57 +0800

    Interface and MySQL learning codeing

--- study/mysql_operate/opmysql.py
+++ study/mysql_operate/opmysql.py
@@ -28,14 +28,12 @@ import pymysql
 
 
 # from public import config
-
 class OperationDbInterface(object):
-    # Define database init connection
-    def __init__(self, host_db="10.64.30.182", user_db="test",
+    def __init__(self, host_db="127.0.0.1", user_db="root",
                  password_db="seevision", name_db="test_interface",
                  port_db=3306, link_type=0):
         """
-
+        Define database init connection
         @param host_db:database service host, if mysql in local, the host_db is the pc ip address
         @param user_db:database user name
         @param password_db:database password
@@ -65,11 +63,76 @@ class OperationDbInterface(object):
             self.cur = self.conn.cursor()
         except pymysql.Error as e:
             print("Create database connection failed | Mysql Error %d: %s" % (e.args[0], e.args[1]))
-            logging.basicConfig(filename=config.src_path + "/log/system_error.log", level=logging.DEBUG,
+            self.log_print(e, level=logging.DEBUG)
+
+    def log_print(self, e, level=logging.DEBUG):
+        """
+        log saved statement
+        @param e: deliver a exception into for saving log
+        @param level: deliver the log's level
+        @return:None
+        """
+        if not os.path.exists(config.src_path + "/log"):
+            os.mkdir(config.src_path + "/log")
+            print("Create log folder!")
+        if e:
+            logging.basicConfig(filename=config.src_path + "/log/system_error.log", level=level,
                                 format="%(asctime)s_%(filename)s[line:%(lineno)d]_%(levelname)s %(message)s")
             logger = logging.getLogger(__name__)
             logger.exception(e)
 
+    def op_sql(self, condition):
+        """
+        Define single statement's operation, include delete、update.
+        @param condition:SQL statement, the common use function can replace updateone\deleteone
+        @return:dictionary type
+        """
+        try:
+            # execute SQL statement
+            self.cur.execute(condition)
+            # commit cursor data
+            self.conn.commit()
+            result = {"code": "0000", "message": "Operation execute success", "data": []}
+        except pymysql.Error as e:
+            self.conn.rollback()
+            result = {"code": "9999", "message": "Operation execute abnormal", "data": []}
+            print("Database failure | op_sql %d: %s" % (e.args[0], e.args[1]))
+            self.log_print(e, level=logging.DEBUG)
+        return result
+
+    def select_one(self, condition):
+        """
+        single query data
+        @param condition:SQL statement
+        @return:dictionary type
+        """
+        try:
+            rows_affect = self.cur.execute(condition)
+            # if query result count is bigger than zero
+            if rows_affect > 0:
+                # gain a result
+                result = self.cur.fetchone()
+                result = {"code": "0000", "message": "Single query operation execute success", "data": result}
+            else:
+                result = {"code": "0000", "message": "Single query operation execute success", "data": []}
+        except pymysql.Error as e:
+            self.conn.rollback()
+            result = {"code": "9999", "message": "Single query operation execute abnormal", "data": []}
+            print("Database failure | select_one %d: %s" % (e.args[0], e.args[1]))
+            self.log_print(e, level=logging.DEBUG)
+        return result
+
 
 if __name__ == '__main__':
     print(config.src_path)
+    a = OperationDbInterface()
+    result = a.select_one("select * from config_total where id=1")
+    for key, value in result["data"].items():
+        print("{} : {}".format(key, value))
+    # a.op_sql("delete from config_total where id>1")
+    # a.op_sql("alter table config_total auto_increment=1")
+    # a.op_sql(
+    #     "insert into config_total (key_config, value_config, description, status) values ('test', 'value_test', '测试配置', '1')")
+    # for i in range(9999):
+    #     a.op_sql(
+    #         "insert into config_total (key_config, value_config, description, status) values ('test', 'value_test', '测试配置', '1')")
