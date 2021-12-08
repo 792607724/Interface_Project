6d4691ce7ecd0fafa9d8c5dd1f342e599772e991
Bruce <39899119+792607724@users.noreply.github.com>
2021-12-08 15:23:02 +0800

    Interface and MySQL learning codeing remember insert into zhihu.com

--- SeevisionProject/android/v1.00/doc/html/search/classes_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mainactivity_0',['MainActivity',['../classcom_1_1example_1_1usbhid_1_1MainActivity.html',1,'com::example::usbhid']]]
-];
--- SeevisionProject/android/v1.00/doc/html/search/classes_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['zoom_5ftype_0',['ZOOM_TYPE',['../classcom_1_1seevi_1_1SVCameraDevice_1_1SVCameraDevice_1_1ZOOM__TYPE.html',1,'com::seevi::SVCameraDevice::SVCameraDevice']]]
-];
