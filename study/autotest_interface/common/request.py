9772e4925f2e46ab8e87c1fb0ea80bca4cdfcac3
Bruce <39899119+792607724@users.noreply.github.com>
2021-12-10 15:41:17 +0800

    Interface study process project sync to MAC OS and make sure opmysql.py and request.py worked normally.

--- study/autotest_interface/common/request.py
+++ study/autotest_interface/common/request.py
@@ -142,7 +142,7 @@ class RequestInterface(object):
 
 
 if __name__ == '__main__':
-    for i in range(100000):
+    for i in range(10):
         # 实例化HTTP请求类..
         test_interface = RequestInterface()
         test_db = opmysql.OperationDbInterface(host_db="127.0.0.1", user_db="root",
